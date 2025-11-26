4f137a8b2e49d2d223606d9342268d96ae6b0d30
Jamous Bitrick <jbitrick@bnt.com>
2025-11-26 17:30:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Left 4 Dead 2 Quiet Weapons
+===========================
+
+Reduce weapon sound by 70%. 
+Done by editing are replacing firing sound files.
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wine ~/.steam/debian-installation/steamapps/common/Left\ 4\ Dead\ 2/bin/vpk.exe quiet_weapons
+cp quiet_weapons.vpk ~/.steam/debian-installation/steamapps/common/Left\ 4\ Dead\ 2/left4dead2/addons/
+ls -alh ~/.steam/debian-installation/steamapps/common/Left\ 4\ Dead\ 2/left4dead2/addons/
